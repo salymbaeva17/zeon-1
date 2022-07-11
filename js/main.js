3b13d19cd4db6bff1cce3a96a66dfd130d408af4
salymbaeva17 <salymbaeva17@gmail.com>
2022-07-11 18:02:08 +0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const headerMenu = document.querySelector('.header__menu')
 const mobileMenu = document.querySelector('.header__mobile-menu')
 const headerLinks = document.querySelectorAll('.header__link')
+const burgerSvg = document.querySelector('.header__mobile-menu svg path')
+
 mobileMenu.addEventListener('click', () => {
     headerMenu.classList.toggle('show')
-})
+    burgerSvg.style.fill = burgerSvg.style.fill==='white'? 'black':'white'})
 headerLinks.forEach(item => {
     item.addEventListener('click', () => {
         headerMenu.classList.remove('show')
